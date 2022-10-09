8a8e7860da35dd23fb1d86a3beb158791166412e
Freemn.network <freemn.network@mailfence.com>
2022-10-09 13:35:20 -0700

    Update App.js

--- Part2/courseinfo/src/App.js
+++ Part2/courseinfo/src/App.js
@@ -1,4 +1,28 @@
+const Parts = () => {
+  return(
+    <>
+      <p>{parts[0].name} {parts[0].exercises}</p>
+      <p>{parts[1].name} {parts[1].exercises}</p>
+      <p>{parts[2].name} {parts[2].exercises}</p>
+    </>
+  )
+}
 
+
+const Content = () => {
+  return(
+    <Parts/>
+  )
+}
+
+const Course = () => {
+  return(
+    <>
+      <Header heading={course.name}/>
+      <Content parts={course.parts}/>
+    </>
+  )
+}
 
 const App = () => {
     const course = {
