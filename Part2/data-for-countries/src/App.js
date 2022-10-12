566e99c1531b352ea0e3121111ab49083019ba92
Freemn.network <freemn.network@mailfence.com>
2022-10-12 11:09:33 -0700

    failed

--- Part2/data-for-countries/src/App.js
+++ Part2/data-for-countries/src/App.js
@@ -1,27 +1,30 @@
-import { useEffect, useState } from 'react';
+import React from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-function App() {
-  const [query, setQuery] = useState([])
-  const [countries, setCountries]= useState('')
+const [countries, setCountries] = useState([])
 
-  useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then(res => {
-      
-      setCountries(res.data)
-    })
-  },[])
 
-  const inputHanlder = (event) => {
-    setQuery(event.target.value)
-  }
+useEffect(() => {
+  axios.get("https://restcountries.com/v3.1/all").then(response => 
+    setCountries(
+      response.data.map(({ name, capital, area, languages, flags }) => ({
+        name: name.common,
+        capital,
+        area,
+        languages,
+        flags
+      }))
+    )
+  )
+}, [])
 
-  console.log(countries.filter(item => item.toLowerCase().includes(query)))
-  return(
-    <div className="App">
-      <input type="text" onChange={inputHanlder} />
-    </div>
-  );
+console.log(countries)
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
